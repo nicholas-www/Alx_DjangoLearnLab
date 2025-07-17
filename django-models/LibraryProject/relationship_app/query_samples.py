ea1a907ac448625bbf632c863111a70e02868dd2
Nicholas Amoo <nickewusi99@gmail.com>
2025-07-17 09:57:22 +0000

    Update query_samples

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,11 @@
 from relationship_app.models import Book, Library
 
-library = Library.objects.get(name='Library')
+library_name = ''
+author_name = ''
+
+library = Library.objects.get(name=library_name)
 books = library.books.all()
 
-book = Book.objects.get(author__name='George Orwell')
+book = Book.objects.get(author__name=author_name)
 
 librarian = library.librarian
