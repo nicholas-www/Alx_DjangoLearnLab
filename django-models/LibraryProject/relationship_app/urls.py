231051d0dbcc0077869c5ddb5522c49c56907a83
Nicholas Amoo <nickewusi99@gmail.com>
2025-07-19 14:19:06 +0000

    Updated urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LibraryDetailView
-from .views import list_books
+from .views import RegisterView, LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('books/', list_books, name='books'),
-    path('library/', LibraryDetailView.as_view(), name='library_detail')
+    path('register/', RegisterView.as_view(template_name='relationship_app/register.html'), name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,11 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth import login
+from django.urls import reverse_lazy
+from django.views.generic.edit import FormView
+from django.contrib.auth.views import LogoutView
+
 
 
 from .models import Library, Book
@@ -23,13 +28,28 @@ class LibraryDetailView(ListView):
     context_object_name = 'libraries'
 
 
-def login_view(request):
-    form = AuthenticationForm(request)
+class LoginView(FormView):
+    template_name = 'relationship_app/login.html'
+    form_class = AuthenticationForm
+    success_url = reverse_lazy('home')  # Change 'home' to your desired redirect URL name
 
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
+    def form_valid(self, form):
+        user = form.get_user()
+        login(self.request, user)
+        return super().form_valid(form)
+    
 
-    return render(request, 'relationship_app/login.html')
+class CustomLogoutView(LogoutView):
+    next_page = reverse_lazy('home')
+
+
+
+class RegisterView(FormView):
+    template_name = 'relationship_app/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')  # Redirect after successful registration
+
+    def form_valid(self, form):
+        user = form.save()            # Save the new user
+        login(self.request, user)     # Log the user in immediately
+        return super().form_valid(form)
