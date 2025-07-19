e9708957c63e68b06cfad899abe785d5c563394c
Nicholas Amoo <nickewusi99@gmail.com>
2025-07-19 14:37:58 +0000

    Updated admin view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -81,7 +81,7 @@ def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
 @user_passes_test(is_admin)
-def admin_view(request):
+def Admin(request):
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
