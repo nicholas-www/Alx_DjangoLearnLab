7fb366acbd18cbf48b85deb772e99c29d6a1b0be
Nicholas Amoo <nickewusi99@gmail.com>
2025-07-19 13:59:37 +0000

    Corrected import

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Library, Book
 
