95c88e269e1a728d0fcb75c103f6bfc5da214a24
Nicholas Amoo <nickewusi99@gmail.com>
2025-07-19 14:10:32 +0000

    Created login view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
 
 from .models import Library, Book
 
@@ -17,3 +20,15 @@ class LibraryDetailView(ListView):
     template_name = 'relationship_app/library_detail.html'
     model = Library
     context_object_name = 'libraries'
+
+
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+    return render(request, 'relationship_app/login.html')
