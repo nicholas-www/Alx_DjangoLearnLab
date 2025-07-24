99766bb4756493ce437566a39784b45194b83ea9
Nicholas Amoo <nickewusi99@gmail.com>
2025-07-24 15:54:57 +0000

    Updated views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,5 +4,5 @@ from django.contrib.auth.decorators import permission_required
 # Create your views here.
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_book(request):
+def book_list(request):
     pass
