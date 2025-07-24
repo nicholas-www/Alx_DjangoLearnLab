22a498a4e25b38e36c0ab12977e7095d2188957c
Nicholas Amoo <nickewusi99@gmail.com>
2025-07-24 15:44:08 +0000

    Created CustomeUser model

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 @admin.register(Book)
@@ -7,3 +8,33 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('publication_year', 'author')
     search_fields = ('title', 'author')
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    # Fields shown in the user list view
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    # The fieldsets organize the admin form when editing a user
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "date_of_birth", "profile_photo")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+    # Fields used when creating a new user in the admin
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active"),
+        }),
+    )
+
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+# Register the model and custom admin
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -4,31 +4,4 @@ from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 from django.utils.translation import gettext_lazy as _
 
-class CustomUserAdmin(UserAdmin):
-    model = CustomUser
 
-    # Fields shown in the user list view
-    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
-    list_filter = ("is_staff", "is_superuser", "is_active")
-
-    # The fieldsets organize the admin form when editing a user
-    fieldsets = (
-        (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "date_of_birth", "profile_photo")}),
-        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-
-    # Fields used when creating a new user in the admin
-    add_fieldsets = (
-        (None, {
-            "classes": ("wide",),
-            "fields": ("username", "email", "password1", "password2", "date_of_birth", "profile_photo", "is_staff", "is_active"),
-        }),
-    )
-
-    search_fields = ("username", "email")
-    ordering = ("username",)
-
-# Register the model and custom admin
-admin.site.register(CustomUser, CustomUserAdmin)
