5ac515bc6a06bf08f97f10be3ea2bf1824bf37ff
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-01 10:00:22 +0000

    feat: Set up API routes, views and serializers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import generics
 
+from api.models import Book
+from api.serializers import BookSerializer
 # Create your views here.
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
