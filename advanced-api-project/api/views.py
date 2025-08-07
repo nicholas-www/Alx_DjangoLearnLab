582b0a69a11effc3f53c66371cc9a2b204517672
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-07 16:26:52 +0000

    Implemented ordering func

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django_filters import rest_framework
 
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
 
 from api.models import Book
 from api.serializers import BookSerializer
@@ -13,6 +14,12 @@ class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author']
+    ordering_fields = ['title', 'publication_year']
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
