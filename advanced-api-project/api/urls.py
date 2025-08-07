95b82c3f208c37b4522a878012e1b787e2832864
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-07 15:46:53 +0000

    Updated api endpoints

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('books/', views.ListView.as_view(), name='book-list'),
     path('books/<int:pk>/', views.DetailView.as_view(), name='book-detail'),
-    path('books/add/', views.CreateView.as_view(), name='add-book'),
-    path('books/<int:pk>/update/', views.UpdateView.as_view(), name='update-book'),
+    path('books/create/', views.CreateView.as_view(), name='add-book'),
+    path('books/update/<int:pk>/', views.UpdateView.as_view(), name='update-book'),
     path('books/delete/<int:pk>/', views.DeleteView.as_view(), name='delete-book'),
 ]
