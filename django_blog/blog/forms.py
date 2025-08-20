539a1714c55fee623465b1b9ba85e47d142f5c8c
Nicholas Amoo <nickewusi99@gmail.com>
2025-08-20 16:36:03 +0000

    fix: tag form

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -14,25 +14,12 @@ class RegisterForm(UserCreationForm):
 
 
 class PostForm(forms.ModelForm):
-    tags = forms.CharField(required=False, help_text="Separate tags with commas")
-
     class Meta:
         model = Post
-        fields = ["title", "content", "tags"]
-
-    def save(self, commit=True, *args, **kwargs):
-        instance = super().save(commit=False, *args, **kwargs)
-        if commit:
-            instance.save()
-            # Save tags
-            tag_names = [t.strip() for t in self.cleaned_data["tags"].split(",") if t.strip()]
-            tag_objs = []
-            for name in tag_names:
-                tag_obj, created = Tag.objects.get_or_create(name=name)
-                tag_objs.append(tag_obj)
-            instance.tags.set(tag_objs)
-        return instance
-
+        fields = ["title", "content", "tags"]   # include tags field
+        widgets = {
+            "tags": TagWidget(),               # use TagWidget for nice tag input
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
